172c4802fb03fee6565df6bbbc5aadefa1267050
mendozamorris <mendozamorrismapagmahal@gmail.com>
2026-06-03 21:27:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(e => {
+    if (e.isIntersecting) e.target.classList.add('visible');
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
